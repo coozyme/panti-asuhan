6c4d1818e9aa682846f65f804d1ea15a0b404cbc
Ary Setya Pambudi <programmeranak@gmail.com>
2024-11-18 01:09:05 +0700

    feat: form add tambah anak asuh

--- src/routes/anak-asuh.js
+++ src/routes/anak-asuh.js
@@ -8,5 +8,12 @@ router.get('/', function (req, res) {
    // res.render('landing-page/landing-page.ejs', {
    // });
 })
+router.get('/tambah-anak-asuh', function (req, res) {
+   // res.send('Hello World!')
+   // res.render(path.join(__dirname, '../../src/views/pages/dashboard/dashboard.ejs'));
+   res.render(path.join(__dirname, '../../src/views/pages/anak-asuh/tambah-anak-asuh.ejs'));
+   // res.render('landing-page/landing-page.ejs', {
+   // });
+})
 
 module.exports = router;
