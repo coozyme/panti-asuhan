2bddd870ceebdb349612c75dc1a9d963831f06c6
Ary Setya Pambudi <programmeranak@gmail.com>
2024-11-27 19:52:26 +0700

    feature form add campaign donasi

--- src/routes/campaign.js
+++ src/routes/campaign.js
@@ -8,5 +8,21 @@ router.get('/donasi', function (req, res) {
    // res.render('landing-page/landing-page.ejs', {
    // });
 })
+router.get('/tambah-campaign-donasi', function (req, res) {
+   // res.send('Hello World!')
+   // res.render(path.join(__dirname, '../../src/views/pages/dashboard/dashboard.ejs'));
+   res.render(path.join(__dirname, '../../src/views/pages/campaign/tambah-campaign-donasi.ejs'));
+   // res.render('landing-page/landing-page.ejs', {
+   // });
+})
+router.post('/tambah-campaign-donasi', function (req, res) {
+   // res.send('Hello World!')
+   // res.render(path.join(__dirname, '../../src/views/pages/dashboard/dashboard.ejs'));
+   // res.render(path.join(__dirname, '../../src/views/pages/campaign/tambah-campaign-donasi.ejs'));
+   // res.render('landing-page/landing-page.ejs', {
+   // });
+})
+
+
 
 module.exports = router;
