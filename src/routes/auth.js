8d7bdb1e65f7b1541786b828b79fdc9dda36e5ad
Ary Setya Pambudi <programmeranak@gmail.com>
2024-12-16 02:21:24 +0700

    update landing page

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,20 @@ var router = express.Router();
 const path = require('path');
 const { Login, Register, GeneratePassword, ChangePassword } = require('../controller/auth.js');
 
+router.get('/', function (req, res, next) {
+   if (req.isAuthenticated()) {
+      res.redirect('/dashboard')
+   } else {
+      res.redirect('/auth/login')
+   }
+});
+
 router.get('/login', function (req, res, next) {
-   res.render(path.join(__dirname, '../../src/views/pages/auth/login.ejs'));
+   if (req.isAuthenticated()) {
+      return res.redirect('/dashboard')
+   } else {
+      res.render(path.join(__dirname, '../../src/views/pages/auth/login.ejs'));
+   }
 });
 router.post('/login', Login);
 router.get('/register', Register);
