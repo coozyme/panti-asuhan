2a3d1033dcd9791983a5ed19ce092f94bb11baa6
Ary Setya Pambudi <programmeranak@gmail.com>
2024-12-19 23:53:29 +0700

    update delete rekening

--- src/routes/donasi.js
+++ src/routes/donasi.js
@@ -9,12 +9,5 @@ router.post('/add-manual', AuthMiddleware.isAuthenticated, uploadFile("donasi").
 router.post('/edit-donasi/:id', AuthMiddleware.isAuthenticated, uploadFile("donasi").single('file'), donasi.EditDonasi);
 router.post('/verification/:id/:status', AuthMiddleware.isAuthenticated, donasi.VerificationDonasi);
 router.get('/donatur', AuthMiddleware.isAuthenticated, donasi.GetDonasi);
-router.get('/rekening', function (req, res) {
-   // res.send('Hello World!')
-   // res.render(path.join(__dirname, '../../src/views/pages/dashboard/dashboard.ejs'));
-   res.render(path.join(__dirname, '../../src/views/pages/donasi/rekening.ejs'));
-   // res.render('landing-page/landing-page.ejs', {
-   // });
-})
 
 module.exports = router;
